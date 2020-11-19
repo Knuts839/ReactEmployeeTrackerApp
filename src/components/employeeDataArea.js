6462bb62d404a0e44d22583da424738fa3228cbc
Ravi <knuts839@umn.edu>
2020-11-18 18:20:12 -0600

    set up my data area for my code

--- src/components/employeeDataArea.js
+++ src/components/employeeDataArea.js
@@ -1,7 +1,21 @@
 import React, { useState, useEffect } from "react";
-import DataTable from "./DataTable";
+import DataTable from "./dataTable";
 import Nav from "./Nav";
 import API from "../utils/API";
 import "../styles/DataArea.css";
 import DataAreaContext from "../utils/myDataSpot";
 
+const DataArea = () => {
+    const [developerState, setDeveloperState] = useState({
+      users: [],
+      order: "descend",
+      filteredUsers: [],
+      headings: [
+        { name: "Image", width: "10%", order: "descend" },
+        { name: "name", width: "10%", order: "descend" },
+        { name: "phone", width: "20%", order: "descend" },
+        { name: "email", width: "20%", order: "descend" },
+        { name: "dob", width: "10%", order: "descend" }
+      ]
+    });
+  
