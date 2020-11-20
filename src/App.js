e3e9ced18496007b712c2d6fbd0b6ccf0cb81bfb
Ravi <knuts839@umn.edu>
2020-11-20 10:03:49 -0600

    Wrapperx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 import Main from "./components/Main";
-import Wrapper from "./components/Wrapper";
+import Wrapper from "./components/Wrapperx";
 import Header from "./components/employeeHeader";
 
 import "./App.css";
